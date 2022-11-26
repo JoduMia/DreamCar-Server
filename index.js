d29adf3557fb07024f48a522d8930b2d84874980
ullah <sheikholeei97@gmail.ocm>
2022-11-26 13:22:14 +0600

    category added

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+const connectMongoDb = async () => {
+    try {
+        app.get('/category/:id',async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = {category: id};
+            const result = await oldCarProducts.find(query).toArray();
+            res.send(result);
+        })
 
+    }finally{}
+}
+connectMongoDb().catch(err => console.log(err.message))
 
 
 app.get('/', (req, res) => {
