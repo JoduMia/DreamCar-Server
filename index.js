974fbb2db10f03fa5bb2e2f44fc54737c5c1f58f
ullah <sheikholeei97@gmail.ocm>
2022-11-26 13:20:37 +0600

    mongodb conneted

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cxn7suc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('server is running')
